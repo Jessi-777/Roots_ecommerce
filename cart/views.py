0b30e231b1deb43dc562ec4c1b671350d09343dc
Jessi <7565683-Jessi777@users.noreply.gitlab.com>
2022-03-28 20:02:03 -0700

    incrementing items and subtracting items work. Removing items is functional as well

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from http.client import HTTPResponse
+from itertools import product
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
@@ -54,9 +55,27 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'total': total, 
         'quantity': quantity,
         'cart_items': cart_items,
-        'tax': tax,
+        'tax':tax,
         'grand_total': grand_total,
     }
     return render(request, 'store/cart.html', context)
   
 
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
